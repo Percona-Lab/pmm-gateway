1005e909f02a28c606914c3dfe7b64116d6b7b43
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2018-03-14 09:21:00 +0300

    Replace wsrpc with gRPC.

--- cmd/pmm-gateway/main.go
+++ cmd/pmm-gateway/main.go
@@ -17,57 +17,125 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"log"
+	"math/rand"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/Percona-Lab/wsrpc"
-	"github.com/sirupsen/logrus"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"time"
 
 	"github.com/Percona-Lab/pmm-api/agent"
-	"github.com/Percona-Lab/pmm-api/gateway"
+	"github.com/Percona-Lab/pmm-api/managed"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"gopkg.in/alecthomas/kingpin.v2"
+
 	"github.com/Percona-Lab/pmm-gateway/tunnel"
 )
 
-func handler(rw http.ResponseWriter, req *http.Request) {
-	conn, err := wsrpc.Upgrade(rw, req, nil)
-	if err != nil {
-		logrus.Error(err)
-		http.Error(rw, err.Error(), 400)
-		return
-	}
-	logrus.Infof("Connection from %s.", req.RemoteAddr)
-	defer conn.Close()
+// func handler(rw http.ResponseWriter, req *http.Request) {
+// 	conn, err := wsrpc.Upgrade(rw, req, nil)
+// 	if err != nil {
+// 		logrus.Error(err)
+// 		http.Error(rw, err.Error(), 400)
+// 		return
+// 	}
+// 	logrus.Infof("Connection from %s.", req.RemoteAddr)
+// 	defer conn.Close()
 
-	server := tunnel.NewService(agent.NewServiceClient(conn))
+// 	server := tunnel.NewService(agent.NewServiceClient(conn))
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINFO)
-	go func() {
-		const dial = "127.0.0.1:9100"
-		<-signals
-		logrus.Infof("Creating tunnel to %s", dial)
-		res, err := server.CreateTunnel(&gateway.CreateTunnelRequest{
-			Dial: dial,
-		})
-		if err != nil {
-			logrus.Fatal(err)
-		}
-		logrus.Info(res)
-	}()
+// 	signals := make(chan os.Signal, 1)
+// 	signal.Notify(signals, syscall.SIGINFO)
+// 	go func() {
+// 		const dial = "127.0.0.1:9100"
+// 		<-signals
+// 		logrus.Infof("Creating tunnel to %s", dial)
+// 		res, err := server.CreateTunnel(&gateway.CreateTunnelRequest{
+// 			Dial: dial,
+// 		})
+// 		if err != nil {
+// 			logrus.Fatal(err)
+// 		}
+// 		logrus.Info(res)
+// 	}()
 
-	err = gateway.NewServiceDispatcher(conn, server).Run()
-	logrus.Infof("Server exited with %v", err)
-}
+// 	err = gateway.NewServiceDispatcher(conn, server).Run()
+// 	logrus.Infof("Server exited with %v", err)
+// }
+
+const (
+	shutdownTimeout = 3 * time.Second
+)
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+	log.SetPrefix("stdlog: ")
 	logrus.SetLevel(logrus.DebugLevel)
 	kingpin.Parse()
 
-	http.Handle("/", http.HandlerFunc(handler))
-	const addr = "127.0.0.1:7781"
-	logrus.Infof("Listening on %s...", addr)
-	logrus.Fatal(http.ListenAndServe(addr, nil))
+	l := logrus.WithField("component", "main")
+	defer l.Info("Done.")
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// handle termination signals
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		s := <-signals
+		signal.Stop(signals)
+		l.Warnf("Got %v (%d) signal, shutting down...", s, s)
+		cancel()
+	}()
+
+	gRPCServer := grpc.NewServer()
+	tunnels := tunnel.NewService()
+	agent.RegisterTunnelsServer(gRPCServer, tunnels)
+	managed.RegisterTunnelsServer(gRPCServer, tunnels)
+
+	// TODO FIXME remove this hack for demo
+	{
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINFO)
+		go func() {
+			const dial = "127.0.0.1:9100"
+			<-signals
+			logrus.Infof("Creating tunnel to %s", dial)
+			res, err := tunnels.Create(context.TODO(), &managed.TunnelsCreateRequest{
+				Dial: dial,
+			})
+			if err != nil {
+				logrus.Fatal(err)
+			}
+			logrus.Info(res)
+		}()
+	}
+
+	const addr = "127.0.0.1:8080"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		l.Panic(err)
+	}
+	go func() {
+		for {
+			err = gRPCServer.Serve(listener)
+			if err == nil || err == grpc.ErrServerStopped {
+				break
+			}
+			l.Errorf("Failed to serve: %s", err)
+		}
+		l.Info("Server stopped.")
+	}()
+
+	<-ctx.Done()
+	ctx, cancel = context.WithTimeout(context.Background(), shutdownTimeout)
+	go func() {
+		<-ctx.Done()
+		gRPCServer.Stop()
+	}()
+	gRPCServer.GracefulStop()
+	cancel()
 }
--- tunnel/tunnel.go
+++ tunnel/tunnel.go
@@ -17,115 +17,338 @@
 package tunnel
 
 import (
+	"context"
 	"fmt"
 	"net"
+	"reflect"
+	"strings"
 	"sync"
 
-	"github.com/Percona-Lab/pmm-api/agent"
-	"github.com/Percona-Lab/pmm-api/gateway"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc/metadata"
+
+	"github.com/Percona-Lab/pmm-api/agent"
+	"github.com/Percona-Lab/pmm-api/managed"
 )
 
+type tunnel struct {
+	dial     string
+	listener net.Listener
+	accepted chan *net.TCPConn
+}
+
 type Service struct {
-	client agent.ServiceClient
+	l *logrus.Entry
 
 	rw      sync.RWMutex
-	tunnels map[string]net.Conn
+	tunnels map[string][]tunnel // key is agent UUID
 }
 
-func NewService(client agent.ServiceClient) *Service {
+func NewService() *Service {
 	return &Service{
-		client:  client,
-		tunnels: make(map[string]net.Conn),
+		l:       logrus.WithField("component", "tunnel"),
+		tunnels: make(map[string][]tunnel),
 	}
 }
 
-func (s *Service) runTunnel(c net.Conn, dial string) {
-	defer c.Close()
+func getAgentUUID(ctx context.Context) (string, error) {
+	const h = "pmm-agent-uuid"
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok || len(md[h]) == 0 {
+		return "", fmt.Errorf("no %s metadata", h)
+	}
+	if len(md[h]) > 1 {
+		return "", fmt.Errorf("%s metadata has several values", h)
+	}
+	return strings.ToLower(md[h][0]), nil
+}
 
-	res, err := s.client.CreateTunnel(&agent.CreateTunnelRequest{
-		Dial: dial,
-	})
+func (s *Service) Make(stream agent.Tunnels_MakeServer) error {
+	// TODO move to interceptor
+	uuid, err := getAgentUUID(stream.Context())
 	if err != nil {
-		logrus.Error(err)
-		return
+		return err
 	}
 
-	tunnelID := res.TunnelId
-
-	s.rw.Lock()
-	s.tunnels[tunnelID] = c
-	s.rw.Unlock()
-
+	var dial string
+	var conn *net.TCPConn
 	for {
-		b := make([]byte, 4096)
-		n, err := c.Read(b)
-		if err != nil {
-			logrus.Error(err)
-			return
+		s.rw.RLock()
+		tunnels := s.tunnels[uuid]
+		s.rw.RUnlock()
+
+		cases := make([]reflect.SelectCase, len(tunnels))
+		for i, t := range tunnels {
+			cases[i] = reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(t.accepted),
+			}
 		}
-		if n == 0 {
+		i, v, ok := reflect.Select(cases)
+		if !ok {
+			// channel is closed, we need to get a new list
 			continue
 		}
 
-		res, err := s.client.WriteToTunnel(&agent.WriteToTunnelRequest{
-			TunnelId: tunnelID,
-			Data:     b[:n],
-		})
-		if err != nil {
-			logrus.Error(err)
-			return
-		}
-		if res.Error != "" {
-			logrus.Error(res.Error)
-			return
-		}
-	}
-}
-
-func (s *Service) CreateTunnel(req *gateway.CreateTunnelRequest) (*gateway.CreateTunnelResponse, error) {
-	if req.AgentUuid != "" {
-		return nil, fmt.Errorf("req.AgentUuid (%q) is not handled yet", req.AgentUuid)
+		dial = tunnels[i].dial
+		conn = v.Interface().(*net.TCPConn)
+		break
 	}
 
-	l, err := net.Listen("tcp", "127.0.0.1:0")
+	// send dial message
+	err = stream.Send(&agent.TunnelsEnvelopeFromGateway{
+		Payload: &agent.TunnelsEnvelopeFromGateway_DialRequest{
+			DialRequest: &agent.TunnelsDialRequest{
+				Dial: dial,
+			},
+		},
+	})
 	if err != nil {
-		return &gateway.CreateTunnelResponse{
-			Error: err.Error(),
-		}, nil
+		return err
 	}
 
+	// wait for dial response
+	env, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+	res := env.GetDialResponse()
+	if res == nil {
+		return fmt.Errorf("expected dial response, got %s", env)
+	}
+	if res.Error != "" {
+		return fmt.Errorf("%s", res.Error)
+	}
+
+	var wg sync.WaitGroup
+
+	// receive messages until error, write to TCP connection
+	wg.Add(1)
 	go func() {
+		defer func() {
+			conn.CloseWrite()
+			wg.Done()
+		}()
+
 		for {
-			c, err := l.Accept()
-			if err != nil {
-				logrus.Error(err)
-				continue
+			env, recvErr := stream.Recv()
+			if recvErr != nil {
+				s.l.Error(recvErr)
+				return
 			}
-			go s.runTunnel(c, req.Dial)
+			data := env.GetData()
+			if data == nil {
+				s.l.Errorf("Expected data, got %s.", env)
+				return
+			}
+
+			if len(data.Data) != 0 {
+				s.l.Debugf("Writing %d bytes...", len(data.Data))
+				if _, writeErr := conn.Write(data.Data); writeErr != nil {
+					s.l.Error(writeErr)
+					return
+				}
+			}
+			if data.Error != "" {
+				s.l.Error(data.Error)
+				return
+			}
 		}
 	}()
 
-	return &gateway.CreateTunnelResponse{
-		Listen: l.Addr().String(),
-	}, nil
+	// read from TCP connection until error, send messages
+	wg.Add(1)
+	go func() {
+		defer func() {
+			conn.CloseRead()
+			wg.Done()
+		}()
+
+		for {
+			b := make([]byte, 4096)
+			n, readErr := conn.Read(b)
+			s.l.Debugf("Read %d bytes.", n)
+			var readErrS string
+			if readErr != nil {
+				readErrS = readErr.Error()
+			}
+			env := &agent.TunnelsEnvelopeFromGateway{
+				Payload: &agent.TunnelsEnvelopeFromGateway_Data{
+					Data: &agent.TunnelsData{
+						Error: readErrS,
+						Data:  b[:n],
+					},
+				},
+			}
+			if err := stream.Send(env); err != nil {
+				s.l.Error(err)
+				return
+			}
+			if readErr != nil {
+				s.l.Error(readErr)
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+	return nil
 }
 
-func (s *Service) WriteToTunnel(req *gateway.WriteToTunnelRequest) (*gateway.WriteToTunnelResponse, error) {
-	s.rw.RLock()
-	c := s.tunnels[req.TunnelId]
-	s.rw.RUnlock()
-	if c == nil {
-		return &gateway.WriteToTunnelResponse{Error: fmt.Sprintf("no such tunnel: %s", req.TunnelId)}, nil
-	}
-
-	if _, err := c.Write(req.Data); err != nil {
-		return &gateway.WriteToTunnelResponse{
+func (s *Service) Create(ctx context.Context, req *managed.TunnelsCreateRequest) (*managed.TunnelsCreateResponse, error) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return &managed.TunnelsCreateResponse{
 			Error: err.Error(),
 		}, nil
 	}
-	return &gateway.WriteToTunnelResponse{}, nil
+
+	accepted := make(chan *net.TCPConn)
+	go func() {
+		for {
+			c, err := listener.Accept()
+			if err != nil {
+				s.l.Error(err)
+				close(accepted)
+				return
+			}
+			accepted <- c.(*net.TCPConn)
+		}
+	}()
+
+	s.rw.Lock()
+	tunnels := s.tunnels[req.AgentUuid]
+	tunnels = append(tunnels, tunnel{
+		dial:     req.Dial,
+		listener: listener,
+		accepted: accepted,
+	})
+	s.tunnels[req.AgentUuid] = tunnels
+	s.rw.Unlock()
+
+	return &managed.TunnelsCreateResponse{
+		Listen: listener.Addr().String(),
+	}, nil
 }
 
+func (s *Service) Delete(ctx context.Context, req *managed.TunnelsDeleteRequest) (*managed.TunnelsDeleteResponse, error) {
+	s.rw.Lock()
+	defer s.rw.Unlock()
+
+	for uuid, tunnels := range s.tunnels {
+		for i, t := range tunnels {
+			if t.listener.Addr().String() != req.Listen {
+				continue
+			}
+
+			if err := t.listener.Close(); err != nil {
+				s.l.Error(err)
+			}
+			// TODO close accepted connections
+			tunnels = append(tunnels[:i], tunnels[i+1:]...)
+			if len(tunnels) == 0 {
+				delete(s.tunnels, uuid)
+			} else {
+				s.tunnels[uuid] = tunnels
+			}
+			return &managed.TunnelsDeleteResponse{}, nil
+		}
+	}
+
+	return &managed.TunnelsDeleteResponse{
+		Error: fmt.Sprintf("No tunnel with listen address %s.", req.Listen),
+	}, nil
+}
+
+// func (s *Service) runTunnel(c net.Conn, dial string) {
+// 	defer c.Close()
+
+// 	res, err := s.client.CreateTunnel(&agent.CreateTunnelRequest{
+// 		Dial: dial,
+// 	})
+// 	if err != nil {
+// 		logrus.Error(err)
+// 		return
+// 	}
+
+// 	tunnelID := res.TunnelId
+
+// 	s.rw.Lock()
+// 	s.tunnels[tunnelID] = c
+// 	s.rw.Unlock()
+
+// 	for {
+// 		b := make([]byte, 4096)
+// 		n, err := c.Read(b)
+// 		if err != nil {
+// 			logrus.Error(err)
+// 			return
+// 		}
+// 		if n == 0 {
+// 			continue
+// 		}
+
+// 		res, err := s.client.WriteToTunnel(&agent.WriteToTunnelRequest{
+// 			TunnelId: tunnelID,
+// 			Data:     b[:n],
+// 		})
+// 		if err != nil {
+// 			logrus.Error(err)
+// 			return
+// 		}
+// 		if res.Error != "" {
+// 			logrus.Error(res.Error)
+// 			return
+// 		}
+// 	}
+// }
+
+// func (s *Service) CreateTunnel(req *gateway.CreateTunnelRequest) (*gateway.CreateTunnelResponse, error) {
+// 	if req.AgentUuid != "" {
+// 		return nil, fmt.Errorf("req.AgentUuid (%q) is not handled yet", req.AgentUuid)
+// 	}
+
+// 	l, err := net.Listen("tcp", "127.0.0.1:0")
+// 	if err != nil {
+// 		return &gateway.CreateTunnelResponse{
+// 			Error: err.Error(),
+// 		}, nil
+// 	}
+
+// 	go func() {
+// 		for {
+// 			c, err := l.Accept()
+// 			if err != nil {
+// 				logrus.Error(err)
+// 				continue
+// 			}
+// 			go s.runTunnel(c, req.Dial)
+// 		}
+// 	}()
+
+// 	return &gateway.CreateTunnelResponse{
+// 		Listen: l.Addr().String(),
+// 	}, nil
+// }
+
+// func (s *Service) WriteToTunnel(req *gateway.WriteToTunnelRequest) (*gateway.WriteToTunnelResponse, error) {
+// 	s.rw.RLock()
+// 	c := s.tunnels[req.TunnelId]
+// 	s.rw.RUnlock()
+// 	if c == nil {
+// 		return &gateway.WriteToTunnelResponse{Error: fmt.Sprintf("no such tunnel: %s", req.TunnelId)}, nil
+// 	}
+
+// 	if _, err := c.Write(req.Data); err != nil {
+// 		return &gateway.WriteToTunnelResponse{
+// 			Error: err.Error(),
+// 		}, nil
+// 	}
+// 	return &gateway.WriteToTunnelResponse{}, nil
+// }
+
 // check interfaces
-var _ gateway.ServiceServer = (*Service)(nil)
+var (
+	_ agent.TunnelsServer   = (*Service)(nil)
+	_ managed.TunnelsServer = (*Service)(nil)
+)
